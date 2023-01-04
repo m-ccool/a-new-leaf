8ca322234452f02bc0ea8a4e5587711fde8214a3
B McCool <mccoolcontact@gmail.com>
2023-01-04 01:42:25 -0300

    started routing

--- a-new-leaf/src/components/plant.js
+++ a-new-leaf/src/components/plant.js
@@ -1,5 +1,4 @@
 import { Canvas } from '@react-three/fiber'
-import Plant from './components/plant'
 import { useGLTF, OrbitControls, ContactShadows, Environment } from '@react-three/drei'
 import { useControls } from 'leva'
 
@@ -9,12 +8,19 @@ const MODELS = {
   Snake: 'plant3.gltf'
 }
 
-export default function App() {
+export default function Plant() {
   const { model } = useControls({ model: { value: 'Snake', options: Object.keys(MODELS) } })
   return (
     <>
-      <header>This is a {model.toLowerCase()} plant.</header>
-      <Plant />
+      <Canvas camera={{ position: [0, 10, 10], fov: 10 }}>
+        <hemisphereLight color="white" groundColor="blue" intensity={0.75} />
+        <spotLight position={[10, 10, 10]} angle={0.5} penumbra={1} />
+        <group position={[0, 0, 0]}>
+          <Model position={[0, 0.0, 0]} url={MODELS[model]} />
+          <ContactShadows scale={8} blur={5} far={10} />
+        </group>
+        <OrbitControls autoRotate enableZoom={false} enablePan={false}/>
+      </Canvas>
     </>
   )
 }
